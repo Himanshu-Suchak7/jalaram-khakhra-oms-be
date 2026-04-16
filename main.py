e2d92b2cc5adac86187c69f2cc7b5c32c33ae413
Himanshu-Suchak7 <himanshusuchak.hs@gmail.com>
2026-04-16 15:48:47 +0530

    fix: allow vercel frontend origin in CORS

--- main.py
+++ main.py
@@ -30,7 +30,11 @@ async def favicon():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://jalaram-khakhra-oms-fe.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
